191f80a8e040ded3607ef75d4286c991321e0e89
Christian Held <christian.held@gmx.net>
2022-08-13 21:59:01 +0200

    Add Public API Tests

--- src/AsyncOrderedQueue/QueueItem.cs
+++
@@ -1,9 +0,0 @@
-namespace AsyncOrderedQueue;
-
-internal record class QueueItem<T>(int SequenceNumber, T Value);
-
-internal static class QueueItem
-{
-    public static QueueItem<T> Create<T>(int sequenceNumber, T value) =>
-        new QueueItem<T>(sequenceNumber, value);
-}
---
+++ tests/AsyncOrderedQueue.UnitTests/PublicApiSurfaceTests.cs
@@ -0,0 +1,27 @@
+using PublicApiGenerator;
+
+namespace AsyncOrderedQueue.UnitTests;
+
+public class PublicApiSurfaceTests
+{
+    [Fact]
+    public async Task PublicApiIsUnchanged()
+    {
+        // Arrange
+        const string FileName = "../../../ApprovedApi.txt";
+        var currentApi = typeof(OrderedQueue<>).Assembly.GeneratePublicApi();
+
+        var assemblyVersion = typeof(OrderedQueue<>).Assembly.GetName().Version!;
+
+        // Act
+        if (assemblyVersion.Major > 0 && File.Exists(FileName))
+        {
+            var approvedApi = await File.ReadAllTextAsync(FileName);
+            Assert.Equal(approvedApi, currentApi);
+        }
+        else
+        {
+            await File.WriteAllTextAsync(FileName, currentApi);
+        }
+    }
+}
